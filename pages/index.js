54987902d9926ff96df1a946954e93f8bb276981
ystays <ystay@g.ucla.edu>
2023-08-21 21:10:53 -0700

    Edited profile

--- pages/index.js
+++ pages/index.js
@@ -28,9 +28,9 @@ export default function Home({ allPostsData }) {
           <a href="https://linkedin.com/in/yi-sheng-tay">my LinkedIn page</a>.)
         </p>
         <small>
-          "Years have gone by and I've finally learned to accept myself for who I am: a beggar for good football. I go about the world, hand outstretched, and in the stadiums I plead: 'A pretty move, for the love of God.' 
-          <br></br>
-          And when good football happens, I give thanks for the miracle and I don't give a damn which team or country performs it." -Galeano
+          Senior Computer Science major at UCLA, with an interest in backend development, AI and digital humanities. Previously interned at Intel and Keysight, and has lately been exploring building web apps with the MEAN stack. 
+          <br/>
+          Searching for a new grad SWE role. Looking for stimulating, collaborative work with a supportive team.
         </small>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
